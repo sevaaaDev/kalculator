b7036c9eca0aeff6c221c9123715dc49a77c5136
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-08-16 14:12:49 +0700

    add event listener that assign html value to variable and make function that operate the variable

---
+++ script.js
@@ -0,0 +1,28 @@
+let button = document.querySelectorAll('button')
+
+let operand1 = ''
+let operand2 = ''
+let operator
+let hasil
+
+for (let item of button) {
+  item.addEventListener('click', (e) => {
+    if (e.target.className === 'operand' && operator === undefined) {
+      operand1 += e.target.innerText;
+      console.log(operand1)
+    } else if (e.target.className === 'operand') {
+      operand2 = e.target.innerText;
+      console.log(operand2)
+    } else if (e.target.className === 'operator' && e.target.innerText !== '='){
+      operator = e.target.innerText
+      console.log(operator)
+    } else if (e.target.innerText === '=') {
+      hasil = operate[operator](operand1, operand2)
+    }
+  })
+}
+
+const operate = {
+  '+': (a, b) => +a + +b
+}
+
