8987052bbb487380bbbd9751aad890b2ae3ece6d
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-08-16 14:42:42 +0700

    change variable into an object so i dont have to write it twice

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 let button = document.querySelectorAll('button')
 
-let operand1 = ''
-let operand2 = ''
+let operand =  {
+  first: '',
+  last: '',
+}
 let operator
 let hasil
 
 for (let item of button) {
   item.addEventListener('click', (e) => {
     if (e.target.className === 'operand' && operator === undefined) {
-      operand1 += e.target.innerText;
-      console.log(operand1)
+      operand.first = operand.first + e.target.innerText;
+      console.log(operand.first)
     } else if (e.target.className === 'operand') {
-      operand2 = e.target.innerText;
-      console.log(operand2)
+      operand.last = operand.last + e.target.innerText;
+      console.log(operand.last)
     } else if (e.target.className === 'operator' && e.target.innerText !== '='){
       operator = e.target.innerText
       console.log(operator)
     } else if (e.target.innerText === '=') {
-      hasil = operate[operator](operand1, operand2)
+      hasil = operate[operator](operand.first, operand.last)
       console.log(hasil)
     }
   })
