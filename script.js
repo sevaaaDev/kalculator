ef1be14ea271b9a4fd69e59a4755b107610694a9
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-08-19 13:41:34 +0700

    add keyboard support

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const operate = {
   "×": (a, b) => +a * +b,
 };
 
-const CALC_BTN = document.querySelectorAll('button')
+const CALC_BTN = document.querySelectorAll("button");
 const OPERAND_BTN = document.querySelectorAll(".operand");
 const OPERATOR_BTN = document.querySelectorAll(".operator");
 const DECIMAL_POINT = document.querySelector(".decimal");
@@ -26,40 +26,73 @@ let operand = {
 let operator = "";
 let result;
 
+DECIMAL_POINT.onclick = getDecimal();
+DELETE_BTN.onclick = deleteSomeShit();
+CLEAR_BTN.onclick = clearAllOfIt();
+window.addEventListener("keydown", keyHandler);
 for (let operandBtn of OPERAND_BTN) {
   operandBtn.addEventListener("click", () => {
-    if (operator === "" && operand.first.length < 12) {
-      operand.first = operand.first + operandBtn.innerText;
-      displayNumber(operand.first);
-    } else if (operator !== "" && operand.last.length < 12) {
-      operand.last = operand.last + operandBtn.innerText;
-      displayNumber(operand.last);
-    }
+    getOperand(operandBtn);
   });
 }
-
 for (let operatorBtn of OPERATOR_BTN) {
   operatorBtn.addEventListener("click", () => {
-    if (operator === '' && operatorBtn.innerText !== "=") {
-      operator = operatorBtn.innerText;
-      displayNumber(operator);
-    } else if (operatorBtn.innerText !== "=") {
-      result = operate[operator](operand.first, operand.last);
-      operand.first = String(result);
-      operand.last = "";
-      operator = operatorBtn.innerText;
-      displayNumber(operator);
-    } else if (operatorBtn.innerText === "=" && operator !== "") {
-      result = operate[operator](operand.first, operand.last);
-      operand.first = String(result);
-      operand.last = "";
-      operator = "";
-      displayNumber(getSciNotation(`${result}`));
-    }
+    getOperator(operatorBtn);
   });
 }
 
-DECIMAL_POINT.onclick = () => {
+function keyHandler(e) {
+  let btn = document.querySelector(`button[data-key='${e.key}']`);
+  if (!btn) return;
+  if (btn.className === "operand") {
+    getOperand(btn);
+    return;
+  }
+  if (btn.className === "operator") {
+    getOperator(btn);
+    return;
+  }
+  if (e.key === "Backspace") {
+    deleteSomeShit();
+  }
+  if (e.key === "Escape") {
+    clearAllOfIt();
+  }
+  if (e.key === ".") {
+    getDecimal();
+  }
+}
+
+function getOperand(number) {
+  if (operator === "" && operand.first.length < 12) {
+    operand.first = operand.first + number.innerText;
+    displayNumber(operand.first);
+  } else if (operator !== "" && operand.last.length < 12) {
+    operand.last = operand.last + number.innerText;
+    displayNumber(operand.last);
+  }
+}
+
+function getOperator(op) {
+  if (operator === "" && op.innerText !== "=") {
+    operator = op.innerText;
+    displayNumber(operator);
+  } else if (op.innerText !== "=") {
+    result = operate[operator](operand.first, operand.last);
+    operand.first = String(result);
+    operand.last = "";
+    operator = op.innerText;
+    displayNumber(operator);
+  } else if (op.innerText === "=" && operator !== "" && operand.last !== "") {
+    result = operate[operator](operand.first, operand.last);
+    operand.first = String(result);
+    operand.last = "";
+    operator = "";
+    displayNumber(getSciNotation(`${result}`));
+  }
+}
+
+function getDecimal() {
   if (
     operator === "" &&
     operand.first.length < 12 &&
@@ -75,9 +108,9 @@ DECIMAL_POINT.onclick = () => {
     operand.last = operand.last + DECIMAL_POINT.innerText;
     displayNumber(operand.last);
   }
-};
+}
 
-DELETE_BTN.onclick = () => {
+function deleteSomeShit() {
   if (operator === "") {
     operand.first = operand.first.substring(0, operand.first.length - 1);
     displayNumber(operand.first);
@@ -85,25 +118,20 @@ DELETE_BTN.onclick = () => {
     operand.last = operand.last.substring(0, operand.last.length - 1);
     displayNumber(operand.last);
   }
-};
+}
 
-CLEAR_BTN.onclick = () => {
+function clearAllOfIt() {
   operand.first = "";
   operand.last = "";
   operator = "";
   result = "";
   displayNumber(operand.first);
-};
-
-
-window.addEventListener('keydown', (e) => {
-  let btn = document.querySelector(`data`)
-})
+}
 
 function displayNumber(text) {
   const INPUT_SCREEN = document.querySelector(".input");
-  const OUTPUT_SCREEN = document.querySelector('.output')
-  OUTPUT_SCREEN.innerText = `${operand.first} ${operator} ${operand.last}`
+  const OUTPUT_SCREEN = document.querySelector(".output");
+  OUTPUT_SCREEN.innerText = `${operand.first} ${operator} ${operand.last}`;
   INPUT_SCREEN.innerText = text;
 }
 
@@ -118,6 +146,7 @@ function getSciNotation(result) {
   if (result.length <= 12) return result;
   if (result.includes("+")) return "too big lmao";
   if (result.length > 12 && result.includes(".")) return resultDecimalRounded;
-  if (resultSciNotation.length > 12) return getSciNotation(resultIntegerRounded);
+  if (resultSciNotation.length > 12)
+    return getSciNotation(resultIntegerRounded);
   return resultSciNotation;
 }
